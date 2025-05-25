0c5d9c8466e4e01c6851c588e36ec080a2dbdbe2
Kovy <codingstudent2021@gmail.com>
2025-05-25 14:44:40 +0200

    Adding Team Members Sections to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
+import Members from "./components/Members";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <main>
         <Hero />
         <About />
+        <Members />
       </main>
     </>
   );
