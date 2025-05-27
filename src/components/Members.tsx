0a5496e13502f7fa8e32ecc940f10700fe412d90
Kovy <codingstudent2021@gmail.com>
2025-05-27 17:03:08 +0200

    ProtoType Test - Retrieve Member Data for the Card display

--- src/components/Members.tsx
+++ src/components/Members.tsx
@@ -2,9 +2,13 @@ import { useState, useEffect } from "react";
 import { supabase } from "../lib/supabaseClient";
 
 interface Members {
-  id: string;
+  id: number;
   full_name: string;
   role: string;
+  email: string;
+  phone: string;
+  message: string;
+  profile_img: string;
 }
 
 const Members = () => {
@@ -18,6 +22,7 @@ const Members = () => {
         console.error("Error fetching members:", error);
       } else {
         setMembers(data || []);
+        console.log("DB:", data);
       }
     };
 
@@ -25,18 +30,32 @@ const Members = () => {
   }, []);
 
   return (
-    <div>
-      {members.map((member) => (
-        <div key={member.id} style={{ color: "red" }}>
-          <p>
-            <strong>Name:</strong> {member.full_name}
-          </p>
-          <p>
-            <strong>Role:</strong> {member.role}
-          </p>
-        </div>
-      ))}
-    </div>
+    <section className="members-section">
+      <div className="members-section-intro">
+        <h2 className="section-title">Meet the Team</h2>
+      </div>
+      <div className="members-grid">
+        {members.map(
+          ({ id, full_name, role, email, phone, message, profile_img }) => (
+            <div className="member-card" key={id}>
+              <img
+                src={profile_img}
+                alt="Profile-pic"
+                className="member-image"
+                loading="lazy"
+                style={{ height: 100, width: 100, objectFit: "cover" }}
+              />
+              <h3 style={{ color: "red" }}>{full_name}</h3>
+              <p>{role}</p>
+              <p>{message}</p>
+              <p>{email}</p>
+              <p>{phone}</p>
+              <div className="member-socials"></div>
+            </div>
+          )
+        )}
+      </div>
+    </section>
   );
 };
 
