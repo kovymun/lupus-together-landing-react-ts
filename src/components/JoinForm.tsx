2ba8957f75d1b374599ae951be20f1c55d09a924
Kovy <codingstudent2021@gmail.com>
2025-06-02 20:09:24 +0200

    Adding form styling

--- src/components/JoinForm.tsx
+++ src/components/JoinForm.tsx
@@ -1,38 +1,50 @@
 import { Element } from "react-scroll";
+import "../styles/join.css";
 
 const JoinForm = () => {
   return (
     <Element name="join">
       <section id="join" className="join-section">
         <div className="join-container">
-          <h2>Let's Walk This Journey Together</h2>
+          <h2 className="section-title">Let's Walk This Journey Together</h2>
           <p>
             Join a community that understands your journey - share your story,
             connect with others, and be inspired.
           </p>
+          <div className="container">
+            <form className="join-form">
+              <div className="input-container">
+                <label htmlFor="name">Name</label>
+                <input type="text" id="name" name="name" required />
+              </div>
 
-          <form className="join-form">
-            <label htmlFor="name">Name</label>
-            <input type="text" id="name" name="name" required />
+              <div className="input-container">
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" required />
+              </div>
 
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" required />
+              <div className="input-container">
+                {" "}
+                <label htmlFor="phone">Phone Number (optional)</label>
+                <input type="tel" id="phone" name="phone" />
+              </div>
 
-            <label htmlFor="phone">Phone Number (optional)</label>
-            <input type="tel" id="phone" name="phone" />
+              <div className="input-container">
+                {" "}
+                <label htmlFor="message">Tell us more</label>
+                <textarea id="message" name="message" rows={5} required />
+              </div>
 
-            <label htmlFor="message">Tell us more</label>
-            <textarea id="message" name="message" rows={5} required />
+              <div className="consent">
+                <input type="checkbox" id="consent" required />
+                <label htmlFor="consent">
+                  I agree to be contacted by Lupus Together.
+                </label>
+              </div>
 
-            <div className="consent">
-              <input type="checkbox" id="consent" required />
-              <label htmlFor="consent">
-                I agree to be contacted by Lupus Together.
-              </label>
-            </div>
-
-            <button type="submit">Join the Community</button>
-          </form>
+              <button type="submit">Join the Community</button>
+            </form>
+          </div>
         </div>
       </section>
     </Element>
