67f52c9ff09016fe78ac6ef69f3d16a45836b1aa
Koveshan Munsami <codingstudent2021@gmail.com>
2025-06-12 22:24:14 +0200

    Added Fluid responsive text using css clamp function

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,19 +1,25 @@
-import { Element } from "react-scroll";
+import { Element, Link } from "react-scroll";
 
 const Hero = () => {
   return (
     <Element name="home">
       {" "}
-      <section className="hero" id="hero">
+      <section className="hero" id="hero" style={{ border: "5px solid red" }}>
         <div className="hero-content">
           <h1>Your Story. Your Strength. A Community That Understands</h1>
           <p>
             Join a safe space where people with Lupus connect, share stories,
             and thrive together.
           </p>
-          <a href="#contact" className="hero-cta">
+          <Link
+            to="join"
+            spy={true}
+            smooth={true}
+            duration={600}
+            className="hero-cta"
+          >
             Join Our Community!
-          </a>
+          </Link>
         </div>
         <div className="hero-image">
           <img
