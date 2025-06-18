ef7073886f73525a7997089f63335ad5808197fc
Koveshan Munsami <codingstudent2021@gmail.com>
2025-06-18 15:16:54 +0200

    Testimonial data Migration to BaaS P1 complete

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+import { supabase } from "../lib/supabaseClient";
 import { Element } from "react-scroll";
 import { FaQuoteLeft } from "react-icons/fa";
 import "../styles/stories.css";
@@ -6,83 +8,103 @@ interface Testimonial {
   id: string;
   name: string;
   title: string;
-  avatarUrl: string;
+  avatar: string;
   city: string;
   country: string;
   quote: string;
-  datePosted: string;
+  date_posted: string;
 }
 
-const testimonials: Testimonial[] = [
-  {
-    id: "1",
-    name: "Anke van Tonder",
-    title: "Teacher",
-    avatarUrl: "../src/assets/anke-van-tonder.webp",
-    city: "Cape Town",
-    country: "South Africa",
-    quote:
-      "I felt invisible until I found Lupus Together. This support group helped me feel understood, valued, and no longer completely alone.",
-    datePosted: "12 March 2024",
-  },
-  {
-    id: "2",
-    name: "Michael Johnson",
-    title: "Accountant",
-    avatarUrl: "../src/assets/michael-johnson.webp",
-    city: "Paarl",
-    country: "South Africa",
-    quote:
-      "The community at Lupus Together genuinely cares. Weekly check ins and shared stories gave me strength and purpose again. It's life changing support.",
-    datePosted: "14 February 2024",
-  },
-  {
-    id: "3",
-    name: "Ayesha Hussain",
-    title: "Student",
-    avatarUrl: "../src/assets/ayesha-hussain.webp",
-    city: "Durban",
-    country: "South Africa",
-    quote:
-      "Lupus Together has been a lifeline. After diagnosis, I felt lost, until this group welcomed me and gave me support, guidance, and hope again.",
-    datePosted: "16 January 2024",
-  },
-  {
-    id: "4",
-    name: "Thabo Mokoena",
-    title: "Engineer",
-    avatarUrl: "../src/assets/thabo-mokoena.webp",
-    city: "Pretoria",
-    country: "South Africa",
-    quote:
-      "I joined for my sister. I stayed for the strength. This group's outreach inspires me daily with purpose, love, and real connection.",
-    datePosted: "24 April 2024",
-  },
-  {
-    id: "5",
-    name: "Sophia Kim",
-    title: "Nurse",
-    avatarUrl: "../src/assets/sophia-kim.webp",
-    city: "Boksburg",
-    country: "South Africa",
-    quote:
-      "Lupus Together changed everything. As a nurse, I appreciate their mental health focus. As a patient, I found care, empathy, and belonging.",
-    datePosted: "19 May 2024",
-  },
-  {
-    id: "6",
-    name: "Jane Naidoo",
-    title: "Photographer",
-    avatarUrl: "../src/assets/jane-naidoo.webp",
-    city: "East London",
-    country: "South Africa",
-    quote:
-      "Living with Lupus in silence nearly broke me. This support group helped me feel seen, heard, accepted, and loved. I found strength, community, and hope.",
-    datePosted: "16 December 2023",
-  },
-];
+// const testimonials: Testimonial[] = [
+//   {
+//     id: "1",
+//     name: "Anke van Tonder",
+//     title: "Teacher",
+//     avatarUrl: "../src/assets/anke-van-tonder.webp",
+//     city: "Cape Town",
+//     country: "South Africa",
+//     quote:
+//       "I felt invisible until I found Lupus Together. This support group helped me feel understood, valued, and no longer completely alone.",
+//     datePosted: "12 March 2024",
+//   },
+//   {
+//     id: "2",
+//     name: "Michael Johnson",
+//     title: "Accountant",
+//     avatarUrl: "../src/assets/michael-johnson.webp",
+//     city: "Paarl",
+//     country: "South Africa",
+//     quote:
+//       "The community at Lupus Together genuinely cares. Weekly check ins and shared stories gave me strength and purpose again. It's life changing support.",
+//     datePosted: "14 February 2024",
+//   },
+//   {
+//     id: "3",
+//     name: "Ayesha Hussain",
+//     title: "Student",
+//     avatarUrl: "../src/assets/ayesha-hussain.webp",
+//     city: "Durban",
+//     country: "South Africa",
+//     quote:
+//       "Lupus Together has been a lifeline. After diagnosis, I felt lost, until this group welcomed me and gave me support, guidance, and hope again.",
+//     datePosted: "16 January 2024",
+//   },
+//   {
+//     id: "4",
+//     name: "Thabo Mokoena",
+//     title: "Engineer",
+//     avatarUrl: "../src/assets/thabo-mokoena.webp",
+//     city: "Pretoria",
+//     country: "South Africa",
+//     quote:
+//       "I joined for my sister. I stayed for the strength. This group's outreach inspires me daily with purpose, love, and real connection.",
+//     datePosted: "24 April 2024",
+//   },
+//   {
+//     id: "5",
+//     name: "Sophia Kim",
+//     title: "Nurse",
+//     avatarUrl: "../src/assets/sophia-kim.webp",
+//     city: "Boksburg",
+//     country: "South Africa",
+//     quote:
+//       "Lupus Together changed everything. As a nurse, I appreciate their mental health focus. As a patient, I found care, empathy, and belonging.",
+//     datePosted: "19 May 2024",
+//   },
+//   {
+//     id: "6",
+//     name: "Jane Naidoo",
+//     title: "Photographer",
+//     avatarUrl: "../src/assets/jane-naidoo.webp",
+//     city: "East London",
+//     country: "South Africa",
+//     quote:
+//       "Living with Lupus in silence nearly broke me. This support group helped me feel seen, heard, accepted, and loved. I found strength, community, and hope.",
+//     datePosted: "16 December 2023",
+//   },
+// ];
 
 const Stories = () => {
+  const [stories, setStories] = useState<Testimonial[]>([]);
+
+  console.log(stories);
+
+  useEffect(() => {
+    const fetchStories = async () => {
+      const { data, error } = await supabase
+        .from("testimonials")
+        .select("*")
+        .order("id", { ascending: true });
+      if (error) {
+        console.error("Error fetching testimonials:", error);
+      } else {
+        setStories(data || []);
+      }
+    };
+
+    fetchStories();
+  }, []);
+
   return (
     <Element name="stories">
       <section className="stories-section">
@@ -99,20 +121,20 @@ const Stories = () => {
             </p>
           </div>
           <div className="story-content">
-            {testimonials.map(
+            {stories.map(
               ({
                 id,
                 name,
-                avatarUrl,
+                avatar,
                 title,
                 city,
                 country,
                 quote,
-                datePosted,
+                date_posted,
               }) => (
                 <article key={id} className="story-card">
                   <div className="avatar">
-                    <img src={avatarUrl} alt="Profile-Photo" loading="lazy" />
+                    <img src={avatar} alt="Profile-Photo" loading="lazy" />
                     <div className="avatar-title">
                       <span>{name}</span>
                       <span>{`${title} · ${city}, ${country}`}</span>
@@ -127,7 +149,7 @@ const Stories = () => {
                     />
                     {quote}
                   </blockquote>
-                  <p className="date-posted">- {datePosted}</p>
+                  <p className="date-posted">- {date_posted}</p>
                 </article>
               )
             )}
