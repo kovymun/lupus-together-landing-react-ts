71fb8ea70bbd962d06f78ca71669a7b93f7ae707
Koveshan Munsami <codingstudent2021@gmail.com>
2025-09-25 18:10:53 +0200

    feat: Add API endpoints for community members and register models in admin

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def community_members(request):
+    return HttpResponse("community-member endpoint")
