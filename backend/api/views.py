570de6e66794e46932e79feaa6d7db7503811388
Koveshan Munsami <codingstudent2021@gmail.com>
2025-09-26 16:14:32 +0200

    feat: Implement community members API with GET and POST methods, update models and serializers

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from .serializers import CommunityMemberSerializer
+from .models import CommunityMember
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-# Create your views here.
-
+@api_view(['GET', 'POST'])
 def community_members(request):
-    return HttpResponse("community-member endpoint")
+    # Handle GET requests for community members
+    if request.method == 'GET':
+        # Get all community members from SQLITE3 DB (CommunityMember Table)
+        members = CommunityMember.objects.all()
+        serializer = CommunityMemberSerializer(members, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    # Handle POST requests for community members
+    elif request.method == 'POST':
+        serializer = CommunityMemberSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        print(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
