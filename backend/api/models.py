67eb52fd87245ba353e741702bfb6190bc53b7cc
Koveshan Munsami <codingstudent2021@gmail.com>
2025-09-26 16:14:32 +0200

    feat: Implement community members API with GET and POST methods, update models and serializers

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,12 +7,12 @@ class CommunityMember(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20)
-    message = models.TextField(max_length=50, blank=True, null=True)
+    message = models.TextField(blank=True, null=True)
     consent = models.BooleanField(default=False)
     join_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} joined the Lupus Together community"
+        return f"{self.first_name} {self.last_name}"
     
 """
 Optional Message Field
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,1 +1,8 @@
-#  Serializers file
+#  Community Member Serializer
+from rest_framework import serializers
+from api.models import CommunityMember
+
+class CommunityMemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CommunityMember
+        fields = "__all__"
