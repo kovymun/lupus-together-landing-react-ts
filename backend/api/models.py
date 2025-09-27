d9af7b14b490388895266a5074d29238f23f06bf
Koveshan Munsami <codingstudent2021@gmail.com>
2025-09-27 16:25:11 +0200

    fix: reduce max_length of phone field to 10 characters

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
 
 class CommunityMember(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20)
+    phone = models.CharField(max_length=10)
     message = models.TextField(blank=True, null=True)
     consent = models.BooleanField(default=False)
     join_date = models.DateTimeField(auto_now_add=True)
@@ -14,10 +14,4 @@ class CommunityMember(models.Model):
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
     
-"""
-Optional Message Field
-Validations:
-- blank=True: allows form/serializer to accept empty value.
-- null=True: allows the database to store NULL if no value is provided. (This fields is optional)
-- max_length=50: limiting the user input to 50 characters.
-"""
+
