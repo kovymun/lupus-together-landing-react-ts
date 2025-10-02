0074c14a83829354b4aca0261fd1a4e24f2f1cdc
Koveshan Munsami <codingstudent2021@gmail.com>
2025-10-02 21:58:52 +0200

    feat: Django Migrations_PostgresSQL

--- backend/lupus_backend/settings.py
+++ backend/lupus_backend/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'lupus_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': 'localhost',  # local setup for development
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
 
